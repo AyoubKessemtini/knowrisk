28bb4eb55ced31b331cf8d80165ff308c4a65fcd
aziz-sassi <sassiaziz50@gmail.com>
2024-08-26 17:10:50 +0100

    renamed app

--- ios/KnowlepsyTests/KnowlepsyTests.m
+++ ios/KnowlepsyTests/KnowlepsyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface airoAppTests : XCTestCase
+@interface KnowlepsyTests : XCTestCase
 
 @end
 
-@implementation airoAppTests
+@implementation KnowlepsyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
