4eda6495066847c7390248043d94da4c349496b3
Kessemtini Ayoub <ak@knowlepsy.com>
2024-10-20 15:57:22 -0400

    sleep line chart

--- src/modules/sleep/screens/sleepScreen/index.tsx
+++ src/modules/sleep/screens/sleepScreen/index.tsx
@@ -5,6 +5,7 @@ import { DateSelector } from '@components/DatePicker/DatePicker.tsx';
 import { formatTime } from '@hooks/useDateFormatter.ts';
 import { SleepCard } from '@components/Cards/SleepCard.tsx';
 import { CText } from '@components/CText.tsx';
+import { LineChart } from 'react-native-chart-kit';
 
 export const SleepScreen: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -40,6 +41,43 @@ export const SleepScreen: React.FC = () => {
             title="common.average"
           />
         </View>
+      </View>
+      <View style={styles.wrapper}>
+        <LineChart
+          data={{
+            labels: ['2024-10-18', '2024-10-19', '2024-10-20', '2024-10-21'],
+            datasets: [
+              {
+                data: [87, 65, 49, 98],
+              },
+            ],
+          }}
+          width={380}
+          height={300}
+          fromZero={true}
+          //yAxisMax={100}
+          yAxisSuffix="%"
+          withHorizontalLines={true}
+          withVerticalLines={false}
+          chartConfig={{
+            fillShadowGradientOpacity: 0,
+            backgroundColor: '#ffffff', // White background color
+            backgroundGradientFrom: '#ffffff',
+            backgroundGradientTo: '#ffffff',
+            decimalPlaces: 0, // optional, defaults to 2 decimal places
+            color: () => '#834bbe', // Blue line color
+            labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, // Black label color for better contrast
+            propsForDots: {
+              r: '4',
+              strokeWidth: '1',
+              stroke: '#834bbe',
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 12,
+          }}
+        />
       </View>
     </Screen>
   );
