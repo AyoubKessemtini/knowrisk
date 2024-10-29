7c13354a2ce466f896685937040aa4abda01a2cd
Kessemtini Ayoub <ak@knowlepsy.com>
2024-10-29 16:29:08 +0100

    fetch heart rate historical data

--- src/modules/heartRateDetails/screens/heartRateDetails.tsx
+++ src/modules/heartRateDetails/screens/heartRateDetails.tsx
@@ -1,107 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Screen } from '@components/Screen';
 import { Header } from '@components/Headers/Header.tsx';
 import { CText } from '@components/CText.tsx';
 import { HeartRateAVGCard } from '@modules/heartRateDetails/components/heartRAteAVGCard.tsx';
+import { core } from "@config/Configuration.ts";
 
 interface HeartRateDetails {
   date: string;
-  hrAvg: number;
-  hrvAvg: number;
+  heartRate: number;
+  heartRateVariability: number;
+  day: string;
 }
 
 export const HeartRateDetailsScreen: React.FC = () => {
-  const heartRate: HeartRateDetails[] = [
-    {
-      date: '2024-10-28',
-      hrAvg: 91,
-      hrvAvg: 44,
-    },
-    {
-      date: '2024-10-27',
-      hrAvg: 99,
-      hrvAvg: 40,
-    },
-    {
-      date: '2024-10-26',
-      hrAvg: 88,
-      hrvAvg: 41,
-    },
-    {
-      date: '2024-10-25',
-      hrAvg: 77,
-      hrvAvg: 42,
-    },
-    {
-      date: '2024-10-24',
-      hrAvg: 66,
-      hrvAvg: 43,
-    },
-    {
-      date: '2024-10-23',
-      hrAvg: 77,
-      hrvAvg: 44,
-    },
-    {
-      date: '2024-10-22',
-      hrAvg: 88,
-      hrvAvg: 45,
-    },
-  ];
+  const [heartRate, setHeartRate] = useState<HeartRateDetails[]>([]);
+  const [todayData, setTodayData] = useState<HeartRateDetails | null>(null);
   const today: string = new Date().toISOString().split('T')[0];
-  const todayData = heartRate.filter((data) => data.date === today)[0];
+
+  useEffect(() => {
+    const fetchHeartRateData = async () => {
+      try {
+        const res = await core.getHeartRateWeeklyData.execute();
+        console.log('res', res);
+        const heartRateData: HeartRateDetails[] = res['value'];
+
+        // Find today's data
+        const todayData = heartRateData.find((data) => data.date === today) || null;
+
+        setHeartRate(heartRateData);
+        setTodayData(todayData);
+      } catch (error) {
+        console.error("Error fetching heart rate data:", error);
+      }
+    };
+
+    fetchHeartRateData(); // Call the function
+  }, [today]);
+
   return (
-    <Screen
-      fullscreen
-      withoutTopEdge
-      noHorizontalPadding
-      containerStyles={styles.container}
-    >
-      <Header
-        hasBackButton
-        useCustomBackButton
-        text="common.heartRate"
-        textColor="black"
-      />
-      <View style={styles.wrapper}>
-        <CText size={'lg_light'}>Today</CText>
-        <HeartRateAVGCard
-          title={'Realtime Heart rate'}
-          dateTime={'Now'}
-          value={'76'}
+      <Screen
+          fullscreen
+          withoutTopEdge
+          noHorizontalPadding
+          containerStyles={styles.container}
+      >
+        <Header
+            hasBackButton
+            useCustomBackButton
+            text="common.heartRate"
+            textColor="black"
         />
-        <HeartRateAVGCard
-          title={'Heart rate average'}
-          dateTime={'Today'}
-          value={String(todayData.hrAvg)}
-        />
-        <HeartRateAVGCard
-          title={'Heart rate variability average'}
-          dateTime={'Today'}
-          value={String(todayData.hrvAvg)}
-        />
-        <CText size={'lg_light'}>Past 7 days</CText>
-        {heartRate
-          .filter((data) => data.date !== today)
-          .map((data) => (
-            <View>
-              <HeartRateAVGCard
-                key={data.date}
-                title={'Heart rate average'}
-                dateTime={data.date}
-                value={String(data.hrAvg)}
-              />
-              <HeartRateAVGCard
-                key={data.date}
-                title={'Heart rate variability average'}
-                dateTime={data.date}
-                value={String(data.hrvAvg)}
-              />
-            </View>
-          ))}
-      </View>
-    </Screen>
+        <View style={styles.wrapper}>
+          <CText size={'lg_light'}>Today</CText>
+          <HeartRateAVGCard
+              title={'Realtime Heart rate'}
+              dateTime={'Now'}
+              value={'76'}
+          />
+          {todayData && (
+              <>
+                <HeartRateAVGCard
+                    title={'Heart rate average'}
+                    dateTime={'Today'}
+                    value={String(todayData.heartRate)}
+                />
+                <HeartRateAVGCard
+                    title={'Heart rate variability average'}
+                    dateTime={'Today'}
+                    value={String(todayData.heartRateVariability)}
+                />
+              </>
+          )}
+          <CText size={'lg_light'}>Past 7 days</CText>
+          {heartRate
+              .filter((data) => data.date !== today)
+              .map((data) => (
+                  <View key={data.date}>
+                    <HeartRateAVGCard
+                        title={'Heart rate average'}
+                        dateTime={`${data.day} ${data.date}`}
+                        value={String(data.heartRate)}
+                    />
+                    <HeartRateAVGCard
+                        title={'Heart rate variability average'}
+                        dateTime={`${data.day} ${data.date}`}
+                        value={String(data.heartRateVariability)}
+                    />
+                  </View>
+              ))}
+        </View>
+      </Screen>
   );
 };
 
